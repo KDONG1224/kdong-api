cb3e3e4ab2318529763cdbfe98e6c2aa170344ce
KDONG1224 <gkfl8809@naver.com>
2024-03-03 14:33:36 +0900

    fix: package.json 스크립트 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,15 @@ module.exports = {
       name: 'kdong-api',
       script: 'dist/main.js',
       instances: 'max',
-      exec_mode: 'cluster'
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
   ]
 };
